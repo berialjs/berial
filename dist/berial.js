a286fe7fa202fccf10c1f743a2e121517e89c87c
132yse <1533540012@qq.com>
2020-07-28 21:21:58 +0800

    日常提交

--- dist/berial.js
+++ dist/berial.js
@@ -8,49 +8,66 @@
 
   function define(tag, component, route) {
     class Berial extends HTMLElement {
+      static get name() {
+        return tag
+      }
+
       constructor() {
         super();
-        let template = document.createElement('template');
-        template.innerHTML = `<!-- ${tag} -- ${route} -->`;
+        for (const k in component) {
+          this[k] = component[k];
+        }
+      }
+
+      connectedCallback() {
         this.attachShadow({
           mode: 'open',
-        }).appendChild(template.content.cloneNode(true));
+        });
+
+        apps.push({
+          tag,
+          component,
+          route,
+          element: this,
+        });
+
+        invoke();
       }
     }
-
     const hasDef = window.customElements.get(tag);
     if (!hasDef) {
       customElements.define(tag, Berial);
     }
-
-    apps.push({
-      tag,
-      component,
-      route,
-    });
-
-    return invoke()
   }
 
   function invoke() {
-    const path = window.location.hash || window.location.pathname;
-
     apps.forEach((app) => {
-      defer(() => {
-        const host = document.querySelector(app.tag);
-        if (app.route === path) {
-          app.component.mount(host);
-        } else {
-          app.component.unmount(host);
-        }
+        const host = new Proxy(app.element, {
+          get(target, key) {
+            return target[key]
+          },
+          set(target, key, val) {
+            target[key] = val;
+            process(app, host);
+            return true
+          },
+        });
+        process(app,host);
       });
-    });
+  }
+
+  function process(app, host) {
+    const path = window.location.hash || window.location.pathname || '/';
+
+    if (app.route === path) {
+      app.component.mount(host);
+    } else {
+      app.component.unmount(host);
+    }
   }
 
   window.addEventListener('hashchange', invoke);
   window.addEventListener('popstate', invoke);
-
-  var defer = typeof requestAnimationFrame !== 'undefined' ? requestAnimationFrame : setTimeout;
 
   exports.define = define;
 
