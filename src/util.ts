4654e11e35830ec9c20e9ef667e5cfbea993253e
木杉 <zhmushan@qq.com>
2020-08-01 19:44:14 +0800

    fix syntax error

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,8 @@
-
 import { Lifecycle, Lifecycles } from './types'
 
 export function warn(trigger: string): void
 export function warn(trigger: boolean, msg?: string): void
-export function warn(trigger: any, msg?: any) {
+export function warn(trigger: any, msg?: any): void
 export function warn(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
@@ -12,7 +11,7 @@ export function warn(trigger: boolean | string, msg?: string) {
 
 export function error(trigger: string): void
 export function error(trigger: boolean, msg?: string): void
-export function error(trigger: any, msg?: any) {
+export function error(trigger: any, msg?: any): void
 export function error(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
@@ -33,7 +32,7 @@ export function request(url: string, option?: RequestInit) {
     .then((res) => res.text())
     .then((data) => data)
 }
-  
+
 export function defineProperty(
   target: any,
   key: PropertyKey,
