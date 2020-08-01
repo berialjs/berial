074c65d19c3b5ac8a9befb7c86ee164e32db3d63
H <andywangsy@gmail.com>
2020-08-01 13:27:08 +0800

    Update error.ts

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,9 +1,11 @@
+export function warn(trigger: string)
 export function warn(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
   throw new Error(`[Berial: Warning]: ${msg}`)
 }
 
+export function error(trigger: string)
 export function error(trigger: boolean | string, msg?: string) {
   if (typeof trigger === 'string') msg = trigger
   if (!trigger) return
