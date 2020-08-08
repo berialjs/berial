59521ce71d4bca72ce0e252948d89a8873ade2f2
子文 <kazekage605@gmail.com>
2020-08-08 11:34:14 +0800

    feat: 类型增强 (#32)
    * fix: 修复判断问题
    
    * fix: 修复命名冲突
    
    * feat: 类型增强
    
    * feat: 类型增强
    
    * feat: 类型增强
    
    Co-authored-by: nantao <nantao@deepexi.com>

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import type { App, Lifecycles } from './types'
-
 import { importHtml } from './html-loader'
 import { lifecycleCheck } from './util'
 
@@ -29,28 +28,33 @@ export function register(name: string, entry: string, match: any): void {
   } as App)
 }
 
-export function start(store: any): void {
+export function start(store: any = {}): void {
   started = true
-  reroute(store || {})
+  reroute(store)
 }
 
 function reroute(store: any): Promise<void> {
   const { loads, mounts, unmounts } = getAppChanges()
+
   if (started) {
     return perform()
   } else {
     return init()
   }
+
   async function init(): Promise<void> {
     await Promise.all(loads.map(runLoad))
   }
+
   async function perform(): Promise<void> {
     unmounts.map(runUnmount)
+
     loads.map(async (app) => {
       app = await runLoad(app, store)
       app = await runBootstrap(app)
       return runMount(app)
     })
+
     mounts.map(async (app) => {
       app = await runBootstrap(app)
       return runMount(app)
@@ -66,8 +70,9 @@ function getAppChanges(): {
   const unmounts: App[] = []
   const loads: App[] = []
   const mounts: App[] = []
+
   apps.forEach((app: any) => {
-    const isActive = app.match(window.location)
+    const isActive: boolean = app.match(window.location)
     switch (app.status) {
       case Status.NOT_LOADED:
       case Status.LOADING:
@@ -97,6 +102,7 @@ async function runLoad(app: App, store: any): Promise<any> {
   if (app.loaded) {
     return app.loaded
   }
+
   app.loaded = Promise.resolve().then(async () => {
     app.status = Status.LOADING
     let lifecycle: Lifecycles
@@ -211,6 +217,7 @@ const routingEventsListeningTo = ['hashchange', 'popstate']
 function urlReroute(): void {
   reroute({})
 }
+
 const capturedEvents = {
   hashchange: [],
   popstate: []
@@ -220,6 +227,7 @@ window.addEventListener('hashchange', urlReroute)
 window.addEventListener('popstate', urlReroute)
 const originalAddEventListener = window.addEventListener
 const originalRemoveEventListener = window.removeEventListener
+
 window.addEventListener = function (name: any, fn: any): void {
   if (
     routingEventsListeningTo.indexOf(name) >= 0 &&
@@ -231,6 +239,7 @@ window.addEventListener = function (name: any, fn: any): void {
   // @ts-ignore
   return originalAddEventListener.apply(this, arguments)
 }
+
 window.removeEventListener = function (name: any, fn: any): void {
   if (routingEventsListeningTo.indexOf(name) >= 0) {
     capturedEvents[name] = capturedEvents[name].filter((l: any) => l !== fn)
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,10 @@
-const isArr = (x: unknown) => Array.isArray(x)
-const isObj = (x: unknown) =>
+import { ProxyType } from './types'
+
+const isArr = (x: unknown): boolean => Array.isArray(x)
+const isObj = (x: unknown): boolean =>
   Object.prototype.toString.call(x) === '[object Object]'
 
-export function proxy(base: Record<string, any>, onWirte: any) {
+export function proxy(base: Record<string, any>, onWirte: any): ProxyType {
   const copy = isArr(base) ? [] : getCleanCopy(base)
   let map = Object.create(null)
   let draft = {
@@ -11,19 +13,28 @@ export function proxy(base: Record<string, any>, onWirte: any) {
     onWirte
   }
   return new Proxy(base, {
-    get(target: any, key: string) {
+    get<T extends string, U extends Record<string, any>>(
+      target: U,
+      key: T
+    ): U[T] | boolean {
       if (key === 'IS_BERIAL_SANDBOX') return true
       if (key in map) return map[key]
       if (isObj(base[key]) || isArr(base[key])) {
-        map[key] = proxy(base[key], (obj: object) => (copy[key] = obj))
+        map[key] = proxy(
+          base[key],
+          (obj: Record<string, unknown>) => (copy[key] = obj)
+        )
         return map[key]
       } else {
         return copy[key] || target[key]
       }
     },
-    set(target, key: string, value) {
+    set(target, key: string, value): boolean {
       if (isObj(base[key]) || isArr(base[key])) {
-        map[key] = proxy(value, (obj: object) => (copy[key] = obj))
+        map[key] = proxy(
+          value,
+          (obj: Record<string, unknown>) => (copy[key] = obj)
+        )
       }
       onWirte && onWirte(draft.onWirte)
       copy[key] = value
@@ -32,6 +43,6 @@ export function proxy(base: Record<string, any>, onWirte: any) {
   })
 }
 
-function getCleanCopy(obj: object) {
+function getCleanCopy(obj: Record<string, unknown>) {
   return Object.create(Object.getPrototypeOf(obj))
 }
