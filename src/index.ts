6a1854d6266bb3562f91b157f2111057de0356dd
132yse <1533540012@qq.com>
2020-08-15 09:18:45 +0800

    export mixin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { start, register } from './app'
+import { start, register, mixin, use } from './app'
 import { loadScript, importHtml } from './html-loader'
 import { proxy } from './proxy'
 
-export const Berial = { start, register, loadScript, importHtml, proxy }
+export const Berial = {
+  start,
+  register,
+  loadScript,
+  importHtml,
+  proxy,
+  mixin,
+  use
+}
 
 export default Berial
 
-export { start, register, loadScript, importHtml, proxy }
+export { start, register, loadScript, importHtml, proxy, mixin, use }
