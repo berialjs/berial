3f012858ea158ba9ca2e851eda9ee73615247b9c
Johnson <yeslee1126@gmail.com>
2020-08-21 15:52:27 +0800

    add vue-router

--- example/child-vue/App.vue
+++ example/child-vue/App.vue
@@ -1,27 +1,7 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <img src='https://cn.vuejs.org/images/logo.png' />
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view />
   </div>
 </template>
-
-<script>
-import { setInterval } from 'timers'
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Hello Vue'
-    }
-  },
-  beforeDestroy () {
-    console.log('beforeDestroy')
-  }
-}
-</script>
-
-<style>
-  h1 {
-    color: #42b983;
-  }
-</style>
---
+++ example/child-vue/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <img src='https://cn.vuejs.org/images/logo.png' />
+  </div>
+</template>
+
+<script>
+import { setInterval } from 'timers'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Vue'
+    }
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  }
+}
+</script>
+
+<style>
+  h1 {
+    color: #42b983;
+  }
+</style>
