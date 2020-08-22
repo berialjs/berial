4c266054f73574e505c3179790fce75675d1c80e
07akioni <07akioni2@gmail.com>
2020-08-23 00:30:11 +0800

    test(sandbox): cases for assign on window & run different scripts

--- src/__tests__/sandbox.spec.ts
+++ src/__tests__/sandbox.spec.ts
@@ -1,7 +1,9 @@
 import * as chai from 'chai'
 import { run } from '../sandbox'
+import sinon from 'sinon'
 
-const expect = chai.expect
+const { expect } = chai
+const { spy } = sinon
 
 describe('sandbox', () => {
   it('should throw error when code has dynamic import', () => {
@@ -12,5 +14,29 @@ describe('sandbox', () => {
       err = e
     }
     expect(err?.message).to.eq('Dynamic imports are blocked')
+  })
+  it('should not leak variable on window', () => {
+    run('window.a = 1')
+    expect(!Reflect.has(window, 'a'))
+  })
+  it('should not affect window in different run', () => {
+    const window1Spy = spy()
+    const window2Spy = spy()
+    run(`window.a = 1; console.log(window.a)`, {
+      allowList: {
+        console: {
+          log: window1Spy
+        }
+      }
+    })
+    run(`window.a = 2; console.log(window.a)`, {
+      allowList: {
+        console: {
+          log: window2Spy
+        }
+      }
+    })
+    expect(window1Spy.calledWith(1))
+    expect(window2Spy.calledWith(2))
   })
 })
