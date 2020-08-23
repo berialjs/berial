4316260bf34d3061007f55d0051e31ca2e329b3d
木杉 <zhmushan@qq.com>
2020-08-23 14:21:43 +0800

    bundle dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,35 @@
 import typescript from 'rollup-plugin-typescript2'
 import replace from '@rollup/plugin-replace'
+import dtsPlugin from 'rollup-plugin-dts'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    // {
-    //   file: 'dist/es/berial.esm.js',
-    //   format: 'esm',
-    //   sourcemap: true,
-    //   name: 'berial'
-    // },
-    {
-      file: 'dist/berial.js',
+/**
+ * @param {boolean} dts
+ */
+function createConfig(dts) {
+  let file = `dist/${dts ? 'types/index.d.ts' : 'berial.js'}`
+  return {
+    input: 'src/index.ts',
+    output: {
       format: 'umd',
-      sourcemap: true,
+      file,
+      sourcemap: !dts,
       name: 'berial'
-    }
-  ],
-  plugins: [
-    replace({
-      __DEV__: process.env.NODE_ENV !== 'production'
-    }),
-    typescript({
-      tsconfig: 'tsconfig.json',
-      removeComments: true,
-      useTsconfigDeclarationDir: true
-    })
-  ]
+    },
+    plugins: [
+      !dts && replace({
+        __DEV__: process.env.NODE_ENV !== 'production'
+      }),
+      !dts && typescript({
+        tsconfig: 'tsconfig.json',
+        removeComments: true,
+        useTsconfigDeclarationDir: true
+      }),
+      dts && dtsPlugin()
+    ].filter(Boolean)
+  }
 }
+
+export default [
+  createConfig(),
+  createConfig(true)
+]
