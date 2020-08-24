b42df0732b54084b578193e1a2e2273360196280
伊撒尔 <1533540012@qq.com>
2020-08-24 15:36:06 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,18 @@
 import typescript from 'rollup-plugin-typescript2'
-import replace from '@rollup/plugin-replace'
-import dtsPlugin from 'rollup-plugin-dts'
 
-function createConfig(dts) {
-  let file = `dist/${dts ? 'types/index.d.ts' : 'berial.js'}`
-  return {
-    input: 'src/index.ts',
-    output: {
-      format: 'umd',
-      file,
-      sourcemap: !dts,
-      name: 'berial'
-    },
-    plugins: [
-      !dts && replace({
-        __DEV__: process.env.NODE_ENV !== 'production'
-      }),
-      !dts && typescript({
-        tsconfig: 'tsconfig.json',
-        removeComments: true,
-        useTsconfigDeclarationDir: true
-      }),
-      dts && dtsPlugin()
-    ].filter(Boolean)
-  }
+export default {
+  input: 'src/index.ts',
+  output: {
+    format: 'umd',
+    file,
+    sourcemap: true,
+    name: 'berial',
+  },
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+      removeComments: true,
+      useTsconfigDeclarationDir: true,
+    }),
+  ],
 }
-
-export default [
-  createConfig()
-]
