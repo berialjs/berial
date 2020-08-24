88694059030b5f86f1263d39d00e2162d619dc81
伊撒尔 <1533540012@qq.com>
2020-08-24 10:10:29 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,4 @@ import { mixin, use } from './mixin'
 import { importHtml } from './html-loader'
 import { run } from './sandbox'
 
-export const Berial = {
-  start,
-  register,
-  importHtml,
-  run,
-  mixin,
-  use
-}
-
-export default Berial
-
 export { start, register, importHtml, run, mixin, use }
