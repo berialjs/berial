13c16e015d15065a0801825bed0b36e526e6a29a
132yse <1533540012@qq.com>
2020-11-16 16:34:02 +0800

    整理代码

--- src/entity.ts
+++ src/entity.ts
@@ -1,5 +1,17 @@
-export class Entity extends HTMLElement{
-    constructor(){
-        super()
-    }
+import { inherits } from 'util'
+
+import {importHtml} from './html-loader'
+
+const hostMap = new Map()
+
+export class Entity extends HTMLElement {
+  constructor() {
+    super()
+    init(this)
+  }
+}
+
+async function init(host: any): void {
+    const { lifecycle: selfLife, bodyNode, styleNodes } = await importHtml(host)
+
 }
