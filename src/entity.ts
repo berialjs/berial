9b30c4ae048d926fa463675f93a321ced39d62d3
132yse <1533540012@qq.com>
2020-11-20 20:00:46 +0800

    fix type

--- src/entity.ts
+++ src/entity.ts
@@ -1,6 +1,6 @@
-import { importHtml } from './html-loader'
+import { importHtml, loadScript } from './html-loader'
 
-const hostMap = new Map()
+let root: any = null
 // const stack = window.location.pathname.split('/')
 const stack = ['root', 'a', 'c']
 
@@ -12,17 +12,13 @@ const enum Tags {
 }
 
 export class Entity extends HTMLElement {
+  slots: any
   constructor() {
     super()
-    const template = document.createElement('template')
-    template.innerHTML = `<slot name=''></slot>`
-
-    this.attachShadow({ mode: 'open' }).appendChild(
-      template.content.cloneNode(true)
-    )
+    this.attachShadow({ mode: 'open' })
   }
   connectedCallback(): void {
-    connect(this) // load every entity, wait to load
+    connect(this)
   }
   disconnectedCallback(): void {
     unmount(this)
@@ -31,30 +27,27 @@ export class Entity extends HTMLElement {
 
 async function connect(host: any): Promise<any> {
   if (host.tag & Tags.Connected) return host
-  if (host.path) {
-    const { lifecycle, bodyNode, styleNodes } = await importHtml(host)
-    const frag = document.createDocumentFragment()
-    styleNodes.forEach((s) => frag.appendChild(s))
-    frag.appendChild(bodyNode.content.cloneNode(true))
-    host.shadowRoot.appendChild(frag)
-    host['b-lc'] = lifecycle
-  }
   host['b-p'] = []
   host['b-rc'] = []
-  host['b-lc'].load(host)
 
   // find the neartist parent and push a promise
   let p = host
   while ((p = p.parentNode)) {
     if (p && p['b-p']) {
-      p['b-p'].push(new Promise((r: any) => (host['b-r'] = r)))
-      p['b-rc'].push(mount.bind(null, host))
       host['b-l'] = p['b-l'] + 1
+      if (host.slot === stack[host['b-l'] - 1]) {
+        p['b-p'].push(new Promise((r: any) => (host['b-r'] = r)))
+        p['b-rc'].unshift(mount.bind(null, host))
+      }
       break
     } else {
       host['b-l'] = 1
+      root = host
     }
   }
+
+  load(host)
+
   if (p && p['b-p']) {
     Promise.all(p['b-p']).then((res) => {
       mount(p)
@@ -65,20 +58,48 @@ async function connect(host: any): Promise<any> {
   if (!host.nextElementSibling && !host.firstChild) {
     p['b-rc'].map((r: any) => r())
   }
-  hostMap.set(host.slot || 'root', host)
+
   host.tag |= Tags.Connected
   return host
 }
 
+async function load(host: any): Promise<any> {
+  if (!host.loaded) {
+    if (host.path) {
+      const { lifecycle, bodyNode, styleNodes } = await importHtml(host)
+      const frag = document.createDocumentFragment()
+      styleNodes.forEach((s) => frag.appendChild(s))
+      frag.appendChild(bodyNode.content.cloneNode(true))
+      host.shadowRoot.appendChild(frag)
+      host['b-lc'] = lifecycle
+    } else {
+      const template = document.createElement('template')
+      template.innerHTML = `
+      <style>.path{color: #3f51b5;padding: 10px;}</style>
+      <li class=path> path 参数呢::>_<:: </li>`
+      host.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+    host.loaded = true
+  }
+  const name = stack[host['b-l']]
+  if (name) {
+    const template = document.createElement('template')
+    template.innerHTML = `<slot name='${name}'></slot>`
+    host.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+  if (host.slot === '' || host.slot === stack[host['b-l'] - 1]) {
+    host['b-lc'].load(host)
+  }
+}
+
 function mount(host: any): void {
   if (host.tag & Tags.Mounted) return
-
-  let p = Promise.resolve()
+  let p = Promise.resolve() as any
   p = host['b-lc'].mount(host)
 
   if (p && typeof p.then === 'function') {
-    p.then((res: any) => {
-      host['b-r'] && host['b-r'](res)
+    p.then(() => {
+      host['b-r'] && host['b-r']()
       host.tag |= Tags.Mounted
     })
   } else {
@@ -88,12 +109,12 @@ function mount(host: any): void {
 }
 
 function unmount(host: any): void {
-  host['b-lc'].unmout(host)
+  host['b-lc'].unmount(host)
   host.tag = Tags.Loaded
 }
 
 function reroute(): void {
-  const root = hostMap.get('root')
+  load(root)
 }
 
 const captured = {
